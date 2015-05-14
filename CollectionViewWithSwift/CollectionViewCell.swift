4bfe404cf94648d8b58920f46b97c374de9f0964
VarshaSameer <km.varsha@gmail.com>
2015-05-14 13:06:46 +0530

    Swift Collection View
    Collection View with Add Cell and Delete Cell

---
+++ CollectionViewWithSwift/CollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  CollectionViewCell.swift
+//  CollectionViewWithSwift
+//
+//  Created by Varsha on 28/04/15.
+//  Copyright (c) 2015 URC. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var label : UILabel!
+    
+}
---
+++ CollectionViewWithSwift/ViewController.swift
@@ -0,0 +1,75 @@
+//
+//  ViewController.swift
+//  CollectionViewWithSwift
+//
+//  Created by Varsha on 28/04/15.
+//  Copyright (c) 2015 URC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    var cellNames = [String]();
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        cellNames.append("0")
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    @IBAction func addItem(AnyObject)
+    {
+    var num = String(cellNames.count)
+    cellNames.append(num)
+    let insertIndexPath = NSIndexPath(forItem: cellNames.count - 1, inSection: 0)
+        println("The shopping list contains \(cellNames.count) items.")
+
+    collectionView.insertItemsAtIndexPaths([insertIndexPath])
+    }
+    
+    
+    //UIColloctionView Datasource Protocol methods
+    
+  
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int
+    {
+
+    return cellNames.count
+    }
+    
+    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int
+    {
+    
+    return 1
+    }
+    
+func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+    let cell = collectionView.dequeueReusableCellWithReuseIdentifier("Cell", forIndexPath: indexPath) as! CollectionViewCell
+
+    cell.label.text = cellNames[indexPath.row]
+    
+    return cell;
+    }
+    
+    
+    
+    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath)
+    {
+    
+    cellNames.removeAtIndex(indexPath.row)
+    
+    collectionView.deleteItemsAtIndexPaths([indexPath]);
+    }
+
+
+}
+
